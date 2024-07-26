4925e781560afb6eaf9ca2fe27d35ef0b720934b
Bovic101 <Victorbolanle72@gmail.com>
2024-07-26 22:09:24 +0200

    headerfile update

---
+++ philosopher.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosopher.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vodebunm <vodebunm@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/26 22:00:51 by vodebunm          #+#    #+#             */
+/*   Updated: 2024/07/26 22:08:26 by vodebunm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHER_H
+#define PHILOSOPHER_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+
+
+#endif
